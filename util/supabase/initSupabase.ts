ffc2560e6184fd13ef053f252283f7a5d4494fba
Jhojan Andres Olivarez <jhojanolivares.jo@gmail.com>
2025-05-12 12:39:34 -0400

    change supabase credentials

--- util/supabase/initSupabase.ts
+++ util/supabase/initSupabase.ts
@@ -3,9 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 
 export async function initSupabase() {
   const creds = {
-    PUBLIC_SUPABASE_URL: "https://xulaswsegmeymlufkcid.supabase.co",
+    PUBLIC_SUPABASE_URL: "https://iiqrcxvorwuyapkpfory.supabase.co",
     SUPABASE_ANON_KEY:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1bGFzd3NlZ21leW1sdWZrY2lkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA3MTUwMjgsImV4cCI6MjAzNjI5MTAyOH0.CVl9fjH3T4oYlUr4Fcu1giXyshCqCiiL0B2pRtaKtRs",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpcXJjeHZvcnd1eWFwa3Bmb3J5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2NzU1OTcsImV4cCI6MjA2MjI1MTU5N30.mP7tqVDvnBBtj8dSDd4oToGMfRiK7k9NVFcndXBPE-E",
   };
 
   globalThis.supabase = createBrowserClient(
