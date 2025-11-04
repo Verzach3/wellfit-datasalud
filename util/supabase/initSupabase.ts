3864c786320288fa678ad1c960bbaf465cbe904c
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2025-11-04 13:46:20 -0500

    chore: update supabase creds

--- util/supabase/initSupabase.ts
+++ util/supabase/initSupabase.ts
@@ -1,11 +1,10 @@
 import { createBrowserClient } from "@supabase/ssr";
-import { createClient } from "@supabase/supabase-js";
 
 export async function initSupabase() {
   const creds = {
-    PUBLIC_SUPABASE_URL: "https://xulaswsegmeymlufkcid.supabase.co",
+    PUBLIC_SUPABASE_URL: "https://djctoonetyuckmfvvhlf.supabase.co",
     SUPABASE_ANON_KEY:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1bGFzd3NlZ21leW1sdWZrY2lkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA3MTUwMjgsImV4cCI6MjAzNjI5MTAyOH0.CVl9fjH3T4oYlUr4Fcu1giXyshCqCiiL0B2pRtaKtRs",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqY3Rvb25ldHl1Y2ttZnZ2aGxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2NzAzMjEsImV4cCI6MjA3NzI0NjMyMX0.2YCuLLGzs-RoZdbkrCtrrTO24mioKWvg1URv0bWjFq0",
   };
 
   globalThis.supabase = createBrowserClient(
